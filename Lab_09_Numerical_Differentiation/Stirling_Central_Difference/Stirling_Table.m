163932f7d42aa0cc6759bca9209b3b4a4328bd76
Kamran Janjua <kamranejaz98@gmail.com>
2019-03-29 11:34:06 +0500

    Create Stirling_Table.m

---
+++ Lab_09_Numerical_Differentiation/Stirling_Central_Difference/Stirling_Table.m
@@ -0,0 +1,10 @@
+function table = Stirling_Table(X,Y)
+    n = length(X);
+    table = zeros(n,n); 
+    table(:,1) = Y';
+    for i=2:n,
+      for j=1:n-i+1,
+          table(j,i) = table(j+1,i-1) - table(j,i-1);
+      end
+    end
+end
